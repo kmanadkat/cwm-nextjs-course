c25adfe4b0d4f5e1ba04875f37b56818cbc366e3
kmanadkat <kmanadkat@gmail.com>
2023-09-13 22:12:21 +0530

    feat: Integrated Daisy UI

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const AddToCart = () => {
-	return <div>AddToCart</div>;
+	return (
+		<div>
+			<button className='btn btn-primary'>Add to cart</button>
+		</div>
+	);
 };
 
 export default AddToCart;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 interface IUser {
 	id: number;
 	name: string;
+	email: string;
 }
 
 const UsersPage = async () => {
@@ -14,12 +15,22 @@ const UsersPage = async () => {
 	return (
 		<>
 			<h1>Users</h1>
-			<p>{new Date().toLocaleTimeString()}</p>
-			<ul>
-				{data.map((user) => (
-					<li key={user.id}>{user.name}</li>
-				))}
-			</ul>
+			<table className='table table-bordered'>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					{data.map((user) => (
+						<tr key={user.id}>
+							<td>{user.name}</td>
+							<td>{user.email}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</>
 	);
 };
