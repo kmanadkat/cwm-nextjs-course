0b838c4974234c8a011a71cda941ec34b2ed34b7
kmanadkat <kmanadkat@gmail.com>
2023-09-15 17:44:13 +0530

    feat: Programmatic Navigation

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,18 @@
+'use client';
 import React from 'react';
+import { useRouter } from 'next/navigation';
 
 const NewUserPage = () => {
-	return <div>NewUserPage</div>;
+	const router = useRouter();
+	return (
+		<button
+			className='btn btn-primary'
+			onClick={() => {
+				router.push('/users');
+			}}>
+			Create
+		</button>
+	);
 };
 
 export default NewUserPage;
