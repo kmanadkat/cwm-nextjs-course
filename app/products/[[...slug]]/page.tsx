0ead2242259719c29fa5648a8aacc120d3811fc6
kmanadkat <kmanadkat@gmail.com>
2023-09-15 17:21:22 +0530

    feat: Handline Query Parameters

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 interface Props {
 	params: { slug: string[] };
+	searchParams: { sortOrder: string };
 }
 
-const ProductPage = ({ params: { slug } }: Props) => {
+const ProductPage = ({
+	params: { slug },
+	searchParams: { sortOrder },
+}: Props) => {
 	return (
 		<div>
 			ProductPage
 			<p>{slug}</p>
+			<p>{sortOrder}</p>
 		</div>
 	);
 };
--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { sort } from 'fast-sort';
+import Link from 'next/link';
 
 interface IUser {
 	id: number;
@@ -6,18 +8,28 @@ interface IUser {
 	email: string;
 }
 
-const UserTable = async () => {
+const UserTable = async ({ sortBy }: { sortBy: string }) => {
 	const res = await fetch('https://jsonplaceholder.typicode.com/users', {
 		cache: 'no-store',
 	});
-	const data: IUser[] = await res.json();
+	let data: IUser[] = await res.json();
+
+	if (sortBy == 'name') {
+		data = sort(data).asc((u) => u.name);
+	} else if (sortBy == 'email') {
+		data = sort(data).asc((u) => u.email);
+	}
 
 	return (
 		<table className='table table-bordered'>
 			<thead>
 				<tr>
-					<th>Name</th>
-					<th>Email</th>
+					<th>
+						<Link href='/users?sortOrder=name'>Name</Link>
+					</th>
+					<th>
+						<Link href='/users?sortOrder=email'>Email</Link>
+					</th>
 				</tr>
 			</thead>
 			<tbody>
