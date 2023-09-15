2f5efb2fb36d953cdee361a26ff40890d846fbe3
kmanadkat <kmanadkat@gmail.com>
2023-09-15 18:02:12 +0530

    feat: Handling Not Found Error Pages

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { notFound } from 'next/navigation';
 
 interface Props {
 	params: { id: number };
 }
 
 const UserDetailsPage = ({ params: { id } }: Props) => {
+	if (id > 10) {
+		notFound();
+	}
 	return <div>UserDetailsPage {id}</div>;
 };
 
