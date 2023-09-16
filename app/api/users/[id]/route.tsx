6e0d936fa542350fbca466df8516b5f81af0ae43
kmanadkat <kmanadkat@gmail.com>
2023-09-16 16:07:25 +0530

    feat: Next API Put Request

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -10,3 +10,20 @@ export function GET(
 
 	return NextResponse.json({ id: 1, name: 'Krupesh' });
 }
+
+export async function PUT(
+	request: NextRequest,
+	{ params }: { params: { id: number } }
+) {
+	const body = await request.json();
+	// Basic Validation
+	if (!body.name) {
+		return NextResponse.json({ error: 'name is required' }, { status: 400 });
+	}
+	// Simulate Users Data
+	if (params.id > 10) {
+		return NextResponse.json({ error: 'user not found' }, { status: 404 });
+	}
+
+	return NextResponse.json({ id: 1, name: body.name });
+}
