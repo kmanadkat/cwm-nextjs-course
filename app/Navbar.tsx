10c3c23b9eced714aa4f8575ef8607c6b8d9819a
kmanadkat <kmanadkat@gmail.com>
2023-09-17 15:43:25 +0530

    feat: Integrated Next Auth with Google Provider

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 
 const Navbar = () => {
 	return (
-		<nav className='flex bg-slate-200 py-3 px-5'>
+		<nav className='flex bg-slate-200 py-3 px-5 space-x-5'>
 			<Link href='/' className='mr-5 '>
 				Next.Js
 			</Link>
 			<Link href='/users'>Users</Link>
+			<Link href='/api/auth/signin'>Sign In</Link>
 		</nav>
 	);
 };
