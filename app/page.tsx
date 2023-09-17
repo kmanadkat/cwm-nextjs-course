8cee63c72439b6c48b121547be7cdbe6c79efa6c
kmanadkat <kmanadkat@gmail.com>
2023-09-17 16:03:42 +0530

    feat: Accessing User Auth Session At Server Side

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Link from 'next/link';
+import { getServerSession } from 'next-auth';
+
+import { authOptions } from './api/auth/[...nextauth]/route';
 import { ProductCard } from './components/ProductCard';
 
-export default function Home() {
+export default async function Home() {
+	// Server Side Session Access
+	const session = await getServerSession(authOptions);
+
 	return (
 		<main>
-			<h1>Home Page World</h1>
+			<h1>Hello {session && <span>{session.user?.name}</span>}</h1>
 			<Link href='/users'>Users</Link>
 			<ProductCard />
 		</main>
