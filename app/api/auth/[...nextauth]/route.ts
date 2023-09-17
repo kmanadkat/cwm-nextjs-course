cb3642a9113d68603d0d75dc2803fcc4c202d9c0
kmanadkat <kmanadkat@gmail.com>
2023-09-17 16:33:39 +0530

    feat: Auth with Custom Email & Password

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth, { AuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import bcrypt from 'bcrypt';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import prisma from '@/prisma/client';
 
 export const authOptions: AuthOptions = {
@@ -9,6 +11,34 @@ export const authOptions: AuthOptions = {
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID!,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+		}),
+		CredentialsProvider({
+			name: 'Credentials',
+			credentials: {
+				email: { label: 'Email', type: 'email', placeholder: 'Email' },
+				password: {
+					label: 'Password',
+					type: 'password',
+					placeholder: 'Password',
+				},
+			},
+			async authorize(credentials, req) {
+				if (!credentials?.email || !credentials.password) return null;
+				// Check user
+				const user = await prisma.user.findUnique({
+					where: { email: credentials.email },
+				});
+				if (!user || !user.hashedPassword) return null;
+
+				// Password Check
+				const passwordsMatch = await bcrypt.compare(
+					credentials.password,
+					user.hashedPassword
+				);
+				if (passwordsMatch) return user;
+
+				return null;
+			},
 		}),
 	],
 	session: {
