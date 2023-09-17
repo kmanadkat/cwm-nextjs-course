85416fa94736db5cee032940ac44328a4b203ea0
kmanadkat <kmanadkat@gmail.com>
2023-09-17 22:00:24 +0530

    feat: Local & Server Images

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import { getServerSession } from 'next-auth';
+import Image from 'next/image';
 
 import { authOptions } from './api/auth/[...nextauth]/route';
 import { ProductCard } from './components/ProductCard';
+import ImageWork from '@/public/images/work.jpg';
 
 export default async function Home() {
 	// Server Side Session Access
@@ -13,6 +15,21 @@ export default async function Home() {
 			<h1>Hello {session && <span>{session.user?.name}</span>}</h1>
 			<Link href='/users'>Users</Link>
 			<ProductCard />
+			<Image
+				className='mt-5 rounded-md'
+				src={ImageWork}
+				alt='Working'
+				height={320}
+			/>
+
+			<div className='h-[300px] w-[600px] relative'>
+				<Image
+					className='mt-5 rounded-md object-contain'
+					src={'https://picsum.photos/600/300'}
+					alt='network image'
+					fill
+				/>
+			</div>
 		</main>
 	);
 }
