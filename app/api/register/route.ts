3983e96519cb038abd6adf62bfcf01717c81548d
kmanadkat <kmanadkat@gmail.com>
2023-09-17 16:42:19 +0530

    feat: User Signup with Customer Email & Password

---
+++ app/api/register/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server';
+import bcrypt from 'bcrypt';
+import schema from './schema';
+import prisma from '@/prisma/client';
+
+export async function POST(request: NextRequest) {
+	const body = await request.json();
+	const validation = schema.safeParse(body);
+	if (!validation.success) {
+		return NextResponse.json(validation.error.errors, {
+			status: 400,
+		});
+	}
+
+	const user = await prisma.user.findUnique({
+		where: { email: body.email },
+	});
+	if (user) {
+		return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+	}
+	const hashedPassword = await bcrypt.hash(body.password, 10);
+	const newUser = await prisma.user.create({
+		data: {
+			email: body.email,
+			hashedPassword,
+		},
+	});
+
+	return NextResponse.json({ email: newUser.email });
+}
