50370080717a64868024a9aeba6b3239772006ea
kmanadkat <kmanadkat@gmail.com>
2023-09-17 16:00:49 +0530

    feat: Accessing User Auth Session At Client Side

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,14 +1,25 @@
+'use client';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React from 'react';
+import Loading from './loading';
 
 const Navbar = () => {
+	const { status, data: session } = useSession();
+
 	return (
-		<nav className='flex bg-slate-200 py-3 px-5 space-x-5'>
+		<nav className='flex bg-slate-200 py-3 px-5 justify-between'>
 			<Link href='/' className='mr-5 '>
 				Next.Js
 			</Link>
-			<Link href='/users'>Users</Link>
-			<Link href='/api/auth/signin'>Sign In</Link>
+			<div className='flex space-x-5'>
+				<Link href='/users'>Users</Link>
+				{status === 'loading' && <Loading />}
+				{status === 'unauthenticated' && (
+					<Link href='/api/auth/signin'>Sign In</Link>
+				)}
+				{status === 'authenticated' && <span>{session.user?.name}</span>}
+			</div>
 		</nav>
 	);
 };
